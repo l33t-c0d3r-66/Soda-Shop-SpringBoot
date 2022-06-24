921e125cc5dfae79e954159265bd518d0f4fcb3e
l33t-c0d3r-66 <aliraza.arain.12530@gmail.com>
2022-06-24 19:02:23 +0500

    Initial commit

---
+++ src/main/java/com/sodashop/sodashopspirngboot/SodaShopSpirngBootApplication.java
@@ -0,0 +1,13 @@
+package com.sodashop.sodashopspirngboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodaShopSpirngBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SodaShopSpirngBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sodashop/sodashopspirngboot/SodaShopSpirngBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sodashop.sodashopspirngboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodaShopSpirngBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
